a700234977245de5df82b07178e3bce7f57c03ae
Rajashree Parhi <rajashree.parhi@factset.com>
2023-04-28 23:27:46 +0530

    isolated reducer

---
+++ reducer/MailReducer.js
@@ -0,0 +1,73 @@
+export const mailReducer = (state, action) => {
+    switch (action.type) {
+      case "TOGGLE_READ_UNREAD":
+        return {
+          ...state,
+          emails: state.emails.map((email) =>
+            email.mId === action.payload
+              ? { ...email, unread: !email.unread }
+              : email
+          ),
+        };
+      case "TOGGLE_STAR_UNSTAR":
+        return {
+          ...state,
+          emails: state.emails.map((email) =>
+            email.mId === action.payload
+              ? { ...email, isStarred: !email.isStarred }
+              : email
+          ),
+        };
+      case "TOGGLE_DELETE":
+        return {
+          ...state,
+          emails: state.emails.map((email) =>
+            email.mId === action.payload
+              ? {
+                  ...email,
+                  isTrash: !email.isTrash,
+                  isSpam: email.isSpam ? !email.isSpam : false,
+                }
+              : email
+          ),
+        };
+      case "TOGGLE_SPAM":
+        return {
+          ...state,
+          emails: state.emails.map((email) =>
+            email.mId === action.payload
+              ? {
+                  ...email,
+                  isSpam: !email.isSpam,
+                  isTrash: email.isTrash ? !email.isTrash : false,
+                }
+              : email
+          ),
+        };
+      case "HANDLE_FILTERS":
+        const isChecked = action.payload.target.checked;
+        const filterValue = action.payload.target.value;
+        if (isChecked) {
+          return {
+            ...state,
+            selectedFilters: {
+              ...state.selectedFilters,
+              selectedCheckbox: [...state.selectedFilters.selectedCheckbox, filterValue],
+            },
+          };
+        } else {
+          return {
+            ...state,
+            selectedFilters: {
+              ...state.selectedFilters,
+              selectedCheckbox: state.selectedFilters.selectedCheckbox.filter(
+                (filter) => filter !== filterValue
+              ),
+            },
+          };
+        }
+      default:
+        return state;
+    }
+  };
+  
