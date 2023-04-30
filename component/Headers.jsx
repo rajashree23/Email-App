abac2d8cf72e70351d9ca9c70cd574e833ea06f0
Rajashree Parhi <rajashree.parhi@factset.com>
2023-04-30 18:59:11 +0530

    added email count by category

--- component/Headers.jsx
+++ component/Headers.jsx
@@ -1,20 +1,37 @@
 import { NavLink } from "react-router-dom";
+import { useMailProviderContext } from "../context/MailProvider";
+
+const calculateMailsCountByCategory = (state) =>
+  state.emails.reduce(
+    (countByCategory, currEmail) => {
+      if (currEmail.isSpam) countByCategory["isSpam"]++;
+      else if (currEmail.isTrash) countByCategory["isTrash"]++;
+      else countByCategory["all"]++;
+
+      return countByCategory;
+    },
+    { isSpam: 0, isTrash: 0, all: 0 }
+  );
 
 export const Headers = () => {
   const getActiveClass = ({ isActive }) => (isActive ? "active" : "");
 
+  const { state } = useMailProviderContext();
+  const { isSpam, isTrash, all } = calculateMailsCountByCategory(state);
+
+
   return (
     <nav>
       <NavLink className={`${getActiveClass} link`} to="/">
-        <span class="material-symbols-outlined">mail</span>Inbox
+        <span className="material-symbols-outlined">mail</span>Inbox({all})
       </NavLink>
       <NavLink className={`${getActiveClass} link`} to="/spam">
-        <span class="material-symbols-outlined">error</span>
-        Spam
+        <span className="material-symbols-outlined">error</span>
+        Spam({isSpam})
       </NavLink>
       <NavLink className={`${getActiveClass} link`} to="/trash">
-        <span class="material-symbols-outlined">delete</span>
-        Trash
+        <span className="material-symbols-outlined">delete</span>
+        Trash({isTrash})
       </NavLink>
     </nav>
   );
